2116e0e9d758805b6a0781f4d4bb2c8d32d6a01a
placeholder placeholder <placeholder@mail.com>
2022-01-30 23:08:35 -0500

    more unit tests for hexcms

--- unit_test_hexcms.sh
+++ unit_test_hexcms.sh
@@ -1,20 +1,30 @@
 #! /bin/bash
 # Usage: ./script.sh -b
 
+UnitTest()
+{
+  printf "\n--- Test 1 ---\n\n"
+  python condor_submit.py one_hexcms_file.txt /home/chiarito/unit_test_hexcms/ -d unit_test_hexcms -t -f
+  printf "\n--- Test 2 ---\n\n"
+  python condor_submit.py /DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2/MINIAODSIM -m 1 /cms/chiarito/eos/for_condor/unit_test_hexcms/ -d unit_test_cmslpc -t -f
+  printf "\n--- Test 3 ---\n\n"
+  python condor_submit.py /DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2/MINIAODSIM -m 1 /cms/chiarito/eos/for_condor/unit_test_hexcms/ -d unit_test_cmslpc -t -f --useLFN
+}
 Rebuild()
 {
   echo "Got -b option: Rebuild"
-  python condor_submit.py one_hexcms_file.txt . -d unit_test_hexcms -b -t -f
+  ./cmssw_src_setup.sh
+  UnitTest
 }
 NoRebuild()
 {
   echo "No Rebuild"
-  python condor_submit.py one_hexcms_file.txt . -d unit_test_hexcms -t -f
+  UnitTest
 }
 
 while getopts ":b" option; do
    case $option in
-      b) # display Help
+      b) # Rebuild scratch area
          Rebuild
          exit;;
    esac
