bbb374696ac4d2f30cfde02d5b89f2ceb021433e
Brandon Chiarito <bchiarito@gmail.com>
2022-02-19 20:01:55 -0600

    turn off retries for now

--- helper/template_unpacker.py
+++ helper/template_unpacker.py
@@ -5,7 +5,7 @@ import os
 input_file_filename = '__inputfilefilenamebase__' + '_' + sys.argv[1] + '.dat'
 copy_command = '__copycommand__'
 redirector = "__redirector__"
-RETRIES = 2
+RETRIES = 0
 
 if copy_command == 'NULL':
   print "Unpacker: Got NULL, will not try to copy any files."
@@ -16,15 +16,15 @@ with open(input_file_filename) as f:
     print "Unpacker: command:", copy_command + " " + redirector + line.strip() + " ."
     os.system(copy_command + " " + redirector + line.strip() + " .")
     # retry logic
-    i = line.rfind('/')
-    filename = (line[i+1:len(line)]).strip()
-    ls = os.listdir(".")
-    counter = 0
-    while not filename in ls:
-      if counter == RETRIES:
-        print "\nUnpacker: Reached Retry Limit! Will not try again!\n"
-        break
-      print "\nUnpacker: Copy failed, file not found! Will retry ...\n"
-      print "Unpacker: retry command:", copy_command + " " + redirector + line.strip() + " ."
-      os.system(copy_command + " " + redirector + line.strip() + " .")
-      counter += 1      
+    #i = line.rfind('/')
+    #filename = (line[i+1:len(line)]).strip()
+    #ls = os.listdir(".")
+    #counter = 0
+    #while not filename in ls:
+    #  if counter == RETRIES:
+    #    print "\nUnpacker: Reached Retry Limit! Will not try again!\n"
+    #    break
+    #  print "\nUnpacker: Copy failed, file not found! Will retry ...\n"
+    #  print "Unpacker: retry command:", copy_command + " " + redirector + line.strip() + " ."
+    #  os.system(copy_command + " " + redirector + line.strip() + " .")
+    #  counter += 1      
