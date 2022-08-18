f31bce6036b8c496795021622026603584d2bc8a
Brandon Chiarito <bchiarito@gmail.com>
2022-08-18 09:44:41 -0500

    __name__ protection for stageout and unpacker scripts

--- helper/template_stageout.py
+++ helper/template_stageout.py
@@ -7,9 +7,10 @@ output_location = "__outputlocation__"
 redirector = "__redirector__"
 copy_command = "__copycommand__"
 
-full_command = copy_command + " " + filename + " " + redirector + output_location + "/" + filename.replace('.root', '')+'_'+str(sys.argv[1])+".root"
-print "Stageout: command:", full_command
-stat = int(os.system(full_command))
-if not stat == 0:
-  print "Stageout: FAILURE with exit code", stat
-  raise SystemExit(1)
+if __name__ == "__main__":
+  full_command = copy_command + " " + filename + " " + redirector + output_location + "/" + filename.replace('.root', '')+'_'+str(sys.argv[1])+".root"
+  print "Stageout: command:", full_command
+  stat = int(os.system(full_command))
+  if not stat == 0:
+    print "Stageout: FAILURE with exit code", stat
+    raise SystemExit(1)
--- helper/template_unpacker.py
+++ helper/template_unpacker.py
@@ -7,24 +7,25 @@ copy_command = '__copycommand__'
 redirector = "__redirector__"
 RETRIES = 0
 
-if copy_command == 'NULL':
-  print "Unpacker: Got NULL, will not try to copy any files."
-  sys.exit()
+if __name__ == "__main__":
+  if copy_command == 'NULL':
+    print "Unpacker: Got NULL, will not try to copy any files."
+    sys.exit()
 
-with open(input_file_filename) as f:
-  for line in f:
-    print "Unpacker: command:", copy_command + " " + redirector + line.strip() + " ."
-    os.system(copy_command + " " + redirector + line.strip() + " .")
-    # retry logic
-    #i = line.rfind('/')
-    #filename = (line[i+1:len(line)]).strip()
-    #ls = os.listdir(".")
-    #counter = 0
-    #while not filename in ls:
-    #  if counter == RETRIES:
-    #    print "\nUnpacker: Reached Retry Limit! Will not try again!\n"
-    #    break
-    #  print "\nUnpacker: Copy failed, file not found! Will retry ...\n"
-    #  print "Unpacker: retry command:", copy_command + " " + redirector + line.strip() + " ."
-    #  os.system(copy_command + " " + redirector + line.strip() + " .")
-    #  counter += 1      
+  with open(input_file_filename) as f:
+    for line in f:
+      print "Unpacker: command:", copy_command + " " + redirector + line.strip() + " ."
+      os.system(copy_command + " " + redirector + line.strip() + " .")
+      # retry logic
+      #i = line.rfind('/')
+      #filename = (line[i+1:len(line)]).strip()
+      #ls = os.listdir(".")
+      #counter = 0
+      #while not filename in ls:
+      #  if counter == RETRIES:
+      #    print "\nUnpacker: Reached Retry Limit! Will not try again!\n"
+      #    break
+      #  print "\nUnpacker: Copy failed, file not found! Will retry ...\n"
+      #  print "Unpacker: retry command:", copy_command + " " + redirector + line.strip() + " ."
+      #  os.system(copy_command + " " + redirector + line.strip() + " .")
+      #  counter += 1      
