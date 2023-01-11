173ce484c51ccdb195219b9ffffc11a961ddfd07
Brandon Chiarito <bchiarito@gmail.com>
2023-01-11 16:04:14 -0600

    forgot to include move.sh in previous commit

---
+++ helper/move.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+echo ">>> Starting job on" `date`
+echo ">>> Running on: `uname -a`"
+echo ">>> System software: `cat /etc/redhat-release`"
+echo ""
+echo "&&& Here there are all the input arguments &&&"
+echo $@
+export INITIAL_DIR=$(pwd)
+echo ''
+echo '&&& Current directiory and Contents: &&&'
+pwd
+ls -ldh *
+echo ''
+
+if [ -f /osg/current/setup.sh ]; then
+  echo "&&& Sourcing grid environment for hexcms &&&"
+  source /osg/current/setup.sh
+  echo ''
+fi
+
+echo '&&& run xrdcp &&&'
+echo 'xrdcp -r $1 $2'
+xrdcp -r $1 $2
+
+echo ''
+echo '&&& Finished &&&'
