ffd4590fefcded7a80762da333387110063f6f0e
Brandon Chiarito <bchiarito@gmail.com>
2023-02-05 19:44:06 -0500

    better condor_move, with job dir

--- condor_move.py
+++ condor_move.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import sys
+import os
 import socket
 import argparse
+from datetime import datetime
 
 # import condor modules
 hostname = socket.gethostname()
@@ -23,7 +25,10 @@ except ImportError as err:
 parser = argparse.ArgumentParser(description="", usage="./%(prog)s <source> <destination>\n\nredirector for cmslpc: root://cmseos.fnal.gov/\nredirector for hexcms: root://ruhex-osgce.rutgers.edu/")
 parser.add_argument("source" ,help="path on eos including redirector")
 parser.add_argument("dest", help="path on eos including redirector")
+parser.add_argument("-d", "--dir", dest='jobdir', default='Job_move_'+datetime.now().strftime("%Y-%m-%d-%H-%M-%S"), help="jobdir name")
 args = parser.parse_args()
+
+os.system('mkdir '+args.jobdir)
 
 # define submit file
 sub = htcondor.Submit()
@@ -31,18 +36,26 @@ sub['executable'] = 'helper/move.sh'
 sub['arguments'] = args.source + ' ' + args.dest
 sub['+JobFlavor'] = 'longlunch'
 sub['Notification'] = 'Never'
-sub['use_x509userproxy'] = 'true'
-sub['output'] = '$(Cluster)_$(Process)_out.txt'
-sub['error'] = '$(Cluster)_$(Process)_out.txt'
-sub['log'] = 'log_$(Cluster).txt'
+if site == 'cmslpc': sub['use_x509userproxy'] = 'true'
+if site == 'hexcms': sub['x509userproxy'] = os.path.basename('x509up_u756')
+sub['output'] = args.jobdir+'/$(Cluster)_$(Process)_out.txt'
+sub['error'] = args.jobdir+'/$(Cluster)_$(Process)_out.txt'
+sub['log'] = args.jobdir+'/log_$(Cluster).txt'
 
 collector = htcondor.Collector()
-coll_query = collector.query(htcondor.AdTypes.Schedd, \
-constraint='FERMIHTC_DRAIN_LPCSCHEDD=?=FALSE && FERMIHTC_SCHEDD_TYPE=?="CMSLPC"',
-projection=["Name", "MyAddress"]
-)
-schedd_ad = coll_query[1]
+if site == 'cmslpc':
+  coll_query = collector.query(htcondor.AdTypes.Schedd, \
+    constraint='FERMIHTC_DRAIN_LPCSCHEDD=?=FALSE && FERMIHTC_SCHEDD_TYPE=?="CMSLPC"',
+    projection=["Name", "MyAddress"]
+  )
+  schedd_ad = coll_query[1]
+if site == 'hexcms':
+  schedd_ad = collector.locate(htcondor.DaemonTypes.Schedd) 
 schedd = htcondor.Schedd(schedd_ad)
+
+# make directory
+if site == 'hexcms':
+  os.system('mkdir -p '+args.dest)
 
 # submit
 result = schedd.submit(sub)
