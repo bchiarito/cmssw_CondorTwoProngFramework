863024e3b8ac2577f4df9b99e07bdfd08c6a4455
Brandon Chiarito <bchiarito@gmail.com>
2023-10-30 11:53:13 -0400

    prebuild script changes for nanoaodv9

--- helper/prebuild_setup.sh
+++ helper/prebuild_setup.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
-export SCRAM_ARCH=slc7_amd64_gcc820 # note gcc700 will give error when running pfnano code
+#export SCRAM_ARCH=slc7_amd64_gcc820 # note gcc700 will give error when running pfnano code
 source $VO_CMS_SW_DIR/cmsset_default.sh
 rm -rf prebuild
 mkdir prebuild
 cd prebuild
-scramv1 project CMSSW CMSSW_10_6_20
-cd CMSSW_10_6_20/src
+scramv1 project CMSSW CMSSW_10_6_27
+cd CMSSW_10_6_27/src
 eval `scramv1 runtime -sh`
-git clone https://github.com/bchiarito/cmssw_CustomPFNanoTwoProng.git .
+git clone https://github.com/bchiarito/cmssw_CustomPFNanoTwoProng.git . -b nanoaodv9
 #scram b -j 10
