86daf44926400a84927aac9bc3a88215eaefd729
Brandon Chiarito <bchiarito@gmail.com>
2024-02-01 10:35:43 -0600

    remove -b nanoaodv9 no longer needed

--- helper/prebuild_setup.sh
+++ helper/prebuild_setup.sh
@@ -8,5 +8,5 @@ cd prebuild
 scramv1 project CMSSW CMSSW_10_6_27
 cd CMSSW_10_6_27/src
 eval `scramv1 runtime -sh`
-git clone https://github.com/bchiarito/cmssw_CustomPFNanoTwoProng.git . -b nanoaodv9
+git clone https://github.com/bchiarito/cmssw_CustomPFNanoTwoProng.git .
 #scram b -j 10
